c7c5d06e3f436fbdb33dbf5f0867c2b7a2b7f750
feleco <142535711+feleco@users.noreply.github.com>
2023-08-22 15:43:00 -0300

    Add files via upload
    Jogo mata mosca, html, script, css e todas as fotos para o projeto

---
+++ jogo.js
@@ -0,0 +1,106 @@
+var altura = 0
+var largura = 0
+var vidas = 1
+var tempo = 30
+var criarMosquitoTempo = 1500 
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal'){
+    criarMosquitoTempo = 1500
+
+}else if(nivel === 'dificil'){
+    criarMosquitoTempo = 1000
+}else if(nivel === 'michaeljackson'){
+ criarMosquitoTempo = 750
+}
+
+
+function ajustaTamanhoPalcoJogo() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(largura, altura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval( function(){
+tempo-= 1
+
+if(tempo<0){
+    clearInterval(cronometro)
+    clearInterval(criar)
+    window.location.href = 'vitotia.html'
+}else{
+    document.getElementById('cronometro').innerHTML = tempo
+}
+},1000)
+
+function posicaoRandomica() {
+
+    if(
+        document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+    
+
+     if(vidas>3){
+        location.href='fim_de_jogo.html'
+     }else{
+        document.getElementById('v'+vidas).src = 'imagens/coracao_vazio.png'
+        vidas++
+     }
+    }
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    //criar o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function(){this.remove()}
+    document.body.appendChild(mosquito)
+    console.log(ladoAleatorio())
+}
+
+var criarMosquito = setInterval(
+    function(){
+        posicaoRandomica()
+    },criarMosquitoTempo)
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2)
+
+    switch (classe) {
+        case 0:
+            return 'ladoA'
+
+        case 1:
+            return 'ladoB'
+    }
+}
